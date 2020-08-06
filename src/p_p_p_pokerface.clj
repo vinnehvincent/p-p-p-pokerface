30671b7f9f3298d347313e7e6ad089866ab047fc
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2020-08-06 18:20:26 +0200

    Initial Commit

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,19 +1,27 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card
+        replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (replacements fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= (apply max (vals (frequencies ranks))) 2)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= (apply max (vals (frequencies ranks))) 3)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= (apply max (vals (frequencies ranks))) 4)))
 
 (defn flush? [hand]
   nil)
